34b7ac971b57d88431f1aed6fa96d22663ededfe
guoyff <guoyff@yonyou.com>
2016-03-10 23:30:19 +0800

    添加示例

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// 1
+export SimpleComponent from './01-top-api/SimpleComponent';
+
+// 2
+export ComponentDemo from './02-jsx/ComponentDemo';
+export SpreadDemo from './02-jsx/SpreadDemo';
+export StyleDemo from './02-jsx/StyleDemo';
+
+// 3
+export StateDemo from './03-dataflow/StateDemo';
+export PropsDemo from './03-dataflow/PropsDemo';
+export PropTypesDemo from './03-dataflow/PropTypesDemo';
+
+// 4
+export LifeCycleDemo from './04-lifecycle/LifeCycleDemo';
+export DestroyComponent from './04-lifecycle/DestroyComponent';
+
+// 5
+export HandleDOMComponent from './05-handle-dom/HandleDOMComponent';
---
+++ src/container/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+import {
+  SimpleComponent,
+  ComponentDemo,
+  SpreadDemo,
+  StyleDemo,
+  StateDemo,
+  PropsDemo,
+  PropTypesDemo,
+  LifeCycleDemo,
+  DestroyComponent,
+  HandleDOMComponent
+
+} from '../components';
+
+let data = {
+  name: 'AlphaGo',
+  type: 'robot'
+};
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        {
+          /*
+          <SimpleComponent />
+          <ComponentDemo name="guoyongfeng" />
+          <SpreadDemo {...data} />
+          <StyleDemo />
+          <StateDemo />
+          <PropsDemo title="设置的标题" />
+          <PropTypesDemo />
+          <LifeCycleDemo />
+          <DestroyComponent />
+           */
+        }
+        <HandleDOMComponent />
+
+
+      </div>
+    )
+  }
+}
+
+export default App;
